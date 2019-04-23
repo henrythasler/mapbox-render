e665132289cb7c102d112f1007cd8f3d99473765
Unknown <git@thasler.org>
2019-04-23 21:19:47 +0200

    add some docs and class skeleton

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-mapbox-render
+# Mapbox-Render
+
+This is a Typescript implementation based on mapbox-gl-native to create raster images from any mapbox-gl-style. 
+The main purpose is to render raster tiles for offline/mobile use from my Mapbox-based vectortiles-stack. The secondary purpose is to learn Typescript ;-)
+
+## Prerequisites
+
+- Install node, npm: https://linuxize.com/post/how-to-install-node-js-on-ubuntu-18.04/
+- Install typescript: `sudo npm install -g typescript`
+
+## Typescript interface for mapbox-gl-native
+
+There is none... So I created one on my own based on the information available via Mapbox's github repo.
+
+## Build & Run application
+
+Enable autobuild:
+`tsc -p . --watch`
+
+Run application:
+`nodejs build/render.js`
+
+## References
+
+- https://github.com/mapbox/mapbox-gl-native/tree/master/platform/node
+- https://github.com/consbio/mbgl-renderer
+- https://github.com/klokantech/tileserver-gl
+
+## Typescript References
+
+- https://www.tutorialsteacher.com/typescript
--- src/render.ts
+++ src/render.ts
@@ -4,51 +4,77 @@ import * as sharp from "sharp";
 // import * as path from "path";
 import * as request from "request";
 
-let options:mbgl.MapOptions = {
-  request: function (mapSourceRequest, callback) {
-    request({
-      url: mapSourceRequest.url,
-      encoding: null,
-      gzip: true
-    }, function (err, res, body) {
-      if (err) {
-        callback(err);
-      } else if (res.statusCode == 200) {
-        let response:mbgl.MapSourceResponse = {
-          modified: (res.headers.modified)?new Date(res.headers.modified[0]):undefined, 
-          expires: (res.headers.expires)?new Date(res.headers.expires):undefined, 
-          etag: (res.headers.etag)?res.headers.etag:undefined, 
-          data: body
+let debug: boolean = true;
+
+let options: mbgl.MapOptions = {
+    request: function (mapSourceRequest, callback) {
+        if (debug) {
+            console.log("url=`" + mapSourceRequest.url + "` kind=" + mapSourceRequest.kind)
         }
-        callback(null, response);
-      } else {
-        callback(new Error(JSON.parse(body).message));
-      }
-    });
-  },
-  ratio: 1.0
+
+        request({
+            url: mapSourceRequest.url,
+            encoding: null,
+            gzip: true
+        }, function (err, res, body) {
+            if (err) {
+                callback(err);
+            } else if (res.statusCode == 200) {
+                let response: mbgl.MapSourceResponse = {
+                    modified: (res.headers.modified) ? new Date(res.headers.modified[0]) : undefined,
+                    expires: (res.headers.expires) ? new Date(res.headers.expires) : undefined,
+                    etag: (res.headers.etag) ? res.headers.etag : undefined,
+                    data: body
+                }
+                callback(null, response);
+            } else {
+                callback(new Error(JSON.parse(body).message));
+            }
+        });
+    },
+    ratio: 1.0
 };
 
 var map = new mbgl.Map(options);
 
-import * as style from './cyclemap.json';
+import * as style from '../data/cyclemap.json';
 map.load(<any>style);
 
-map.render({ zoom: 11, width: 512, height: 512, center: [10.5, 47.7] }, function (err, buffer) {
-  if (err) throw err;
+map.render({ zoom: 11, width: 1024, height: 512, center: [12.5, 47.9] }, function (err, buffer) {
+    if (err) throw err;
 
-  map.release();
+    map.release();
 
-  var image = sharp(buffer, {
-    raw: {
-      width: 512,
-      height: 512,
-      channels: 4
-    }
-  });
+    var image = sharp(buffer, {
+        raw: {
+            width: 1024,
+            height: 512,
+            channels: 4
+        }
+    });
 
-  // Convert raw image buffer to PNG
-  image.toFile('image.png', function (err) {
-      if (err) throw err;
+    // Convert raw image buffer to PNG
+    image.toFile('data/image.png', function (err) {
+        if (err) throw err;
     });
 });
+
+
+interface MapboxRenderOptions {
+    accessToken?: string,
+}
+
+
+class MapboxRender {
+    options: MapboxRenderOptions;
+
+    constructor(options: MapboxRenderOptions) {
+        this.options = options;
+    }
+}
+
+
+let mapboxRenderOptions: MapboxRenderOptions = {
+    accessToken: "pk.eyJ1IjoibXljeWNsZW1hcCIsImEiOiJjaXJhYnoxcGEwMDRxaTlubnk3cGZpbTBmIn0.TEO9UhyyX1nFKDTwO4K1xg"
+}
+let mbr = new MapboxRender(mapboxRenderOptions);
